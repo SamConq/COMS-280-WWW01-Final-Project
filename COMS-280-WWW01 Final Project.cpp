467a612752c73a3478e2682786625439fe214b19
SamConq <silvicidal@gmail.com>
2025-04-01 16:28:06 -0400

    4/1/25

--- COMS-280-WWW01 Final Project.cpp
+++ COMS-280-WWW01 Final Project.cpp
@@ -2,14 +2,19 @@
 //
 
 #include <iostream>
+#include <fstream>              // text file library
+
+void createAccount();           // function declarations
+void login();
+bool usernameExists();
 
 void homePage() {               // landing page for the program, user can choose to login or signup, user MUST login to an account before continuing
     
     int userChoice = 0;
 
     do {
-        std::cout << "=================================================\n"; // Landing page for the program
-        std::cout << "      Welcome to Conquest Banking!\n";                // User MUST login to an account before they can continue
+        std::cout << "=================================================\n"; 
+        std::cout << "      Welcome to Conquest Banking!\n";                
         std::cout << "Please login or create an account to get started\n";  
         std::cout << "=================================================\n";
         std::cout << "\n";
@@ -25,15 +30,103 @@ void homePage() {               // landing page for the program, user can choose
         std::cout << "Login";
     }
     if (userChoice == 2) {
-        std::cout << "Signup";
+        createAccount();
     }
     
 }
 
+bool usernameExists(const std::string& username) {         // check if username already exists in the text file
+
+    std::string existingUsername;
+    std::string existingPassword;
+    
+    std::ifstream inFile("accounts.txt");
+
+    if (inFile.is_open()) {
+        while (inFile >> existingUsername >> existingPassword) {
+            if (existingUsername == username) {
+                inFile.close();
+                return true;
+            }
+        }
+        inFile.close();
+    }
+    return false;
+
+}
+
 void createAccount() {          // user can create a brand new account, creating login credentials that are saved to a text file
+    
+    std::string username;           
+    std::string password;           
+    std::string verifyPassword;
+    
+    std::cout << "=================================================\n";
+    std::cout << "      Please enter a username\n";
+    std::cout << "=================================================\n";
+
+    std::cin >> username;                                                   // user creates username
+
+    
+    while (usernameExists(username)) {                                      // check if username already exists
+        std::cout << "=================================================\n";
+        std::cout << "      Username already exists! Try another.\n";
+        std::cout << "=================================================\n";
+        std::cin >> username;
+    }
+
+
+    std::cout << "=================================================\n";
+    std::cout << "      Please enter a password\n";
+    std::cout << "=================================================\n";
+
+    std::cin >> password;                                                   // user creates password
+
+    std::cout << "=================================================\n";
+    std::cout << "      Please confirm your password\n";
+    std::cout << "=================================================\n";
+
+    std::cin >> verifyPassword;                                             // user must confirm their password
+    if (password != verifyPassword) {
+        do {
+            std::cout << "=================================================\n";
+            std::cout << "      Incorrect password, please try again\n";
+            std::cout << "=================================================\n";
+            std::cin >> verifyPassword;
+        } while (password != verifyPassword);
+    }
+
+    
+    std::ofstream outFile("accounts.txt", std::ios::app);                   // open text file in append mode to allow adding multiple accounts
+
+    if (outFile.is_open()) {
+        outFile << username << " " << password << "\n";                     // save credentials
+        outFile.close();
+        std::cout << "=================================================\n";
+        std::cout << "      Account successfully created!\n";
+        std::cout << "=================================================\n";
+    }
+    else {
+        std::cout << "Error: Unable to open file for writing.\n";
+    }
 
 }
 void login() {                  // user can attempt to login to an existing account; if a matching account is found within the accounts text file, they are loggin in successfully
+
+    std::string usernameLog;
+    std::string passwordLog;
+
+    std::cout << "=================================================\n";
+    std::cout << "      Please enter your username\n";
+    std::cout << "=================================================\n";
+
+    std::cin >> usernameLog;                                                   // user enters existing username
+
+    std::cout << "=================================================\n";
+    std::cout << "      Please enter your password\n";
+    std::cout << "=================================================\n";
+
+    std::cin >> passwordLog;                                                   // user enters existing password
 
 }
 
